49b5a85dbb3eca3f351f82f877bde57a3e6fc977
vidhyasagar <vs1522250@gmail.com>
2026-04-27 16:05:36 +0530

    first Gsap

---
+++ GSAP1.2/script.js
@@ -0,0 +1,22 @@
+let tl = gsap.timeline()
+
+tl.from("h2",{
+    y: -30,
+    duration: 0.5,
+    delay: 0.5,
+    opacity: 0
+})
+
+tl.from("h4",{
+    y: -30,
+    duration: 1,
+    opacity: 0,
+    stagger: 0.3
+})
+
+tl.from("h1",{
+    y: 20,
+    duration: 1,
+    scale: 0.2,
+    opacity: 0
+})
