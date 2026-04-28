2cb138602d013b462a01a93fde66d7b9b4b02606
vidhyasagar <vs1522250@gmail.com>
2026-04-28 17:38:00 +0530

    scroll triggers

--- GSAP2/script.js
+++ GSAP2/script.js
@@ -5,14 +5,28 @@ gsap.from(".page1 .box",{
     duartion: 2,
 })
 
-gsap.from(".page2 .box",{
+gsap.from(".page2 h1",{
     scale: 0,
-    rotate: 360,
     duartion: 2,
+    opacity: 0,
+    x: 500,
     scrollTrigger: {
-        trigger: ".page2 .box",
+        trigger: ".page2 h1",
         scroller: "body",
         markers: true,
-        start: "top 60%"
+        start: "top 50%"
+    }
+})
+
+gsap.from(".page2 h2",{
+    scale: 0,
+    duartion: 2,
+    opacity: 0,
+    x: -500,
+    scrollTrigger: {
+        trigger: ".page2 h2",
+        scroller: "body",
+        markers: true,
+        start: "top 50%"
     }
 })
