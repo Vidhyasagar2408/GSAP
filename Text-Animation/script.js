bd4979da5f5a231d1644fdde1b319a3a131f7e75
vidhyasagar <vs1522250@gmail.com>
2026-04-30 01:40:50 +0530

    text animation

--- Text-Animation/script.js
+++ Text-Animation/script.js
@@ -1,22 +1,26 @@
-const marque = gsap.to("#marque", {
-  xPercent: -100,
-  ease: "none",
-  repeat: -1,
-  duration: 2,
-});
+function marqueAnimation() {
+  const marque = gsap.to("#marque", {
+    xPercent: -100,
+    ease: "none",
+    repeat: -1,
+    duration: 2,
+  });
 
-const arrow = gsap.to("#marque img", {
-  rotate: 180,
-});
+  const arrow = gsap.to("#marque img", {
+    rotate: 180,
+  });
 
-window.addEventListener("wheel", (e) => {
-  if (e.deltaY > 0) {
-    scrollDown = true;
-    marque.play();
-    arrow.play();
-  } else if (e.deltaY < 0) {
-    scrollDown = false;
-    marque.reverse();
-    arrow.reverse();
-  }
-});
+  window.addEventListener("wheel", (e) => {
+    if (e.deltaY > 0) {
+      scrollDown = true;
+      marque.play();
+      arrow.play();
+    } else if (e.deltaY < 0) {
+      scrollDown = false;
+      marque.reverse();
+      arrow.reverse();
+    }
+  });
+}
+
+marqueAnimation();
