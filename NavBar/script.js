6d308b0a3af1cc42875b5a5428ecad3255ed860a
vidhyasagar <vs1522250@gmail.com>
2026-04-29 20:03:03 +0530

    Nav Bar

---
+++ NavBar/script.js
@@ -0,0 +1,20 @@
+let tl = gsap.timeline();
+
+tl.to("#full",{
+    right: 0,
+    duration: 0.5
+})
+
+tl.from("#full h4",{
+    x:600,
+    opacity: 0,
+    duration: 0.7,
+    stagger:0.1
+})
+
+tl.from("#full i",{
+    x:100,
+    duration:0.2,
+    opacity: 0
+})
+
