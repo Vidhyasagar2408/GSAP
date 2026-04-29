e8bd045674b78d894ea51b56ff88197c4efc9263
vidhyasagar <vs1522250@gmail.com>
2026-04-29 20:13:29 +0530

    Nav Bar

--- NavBar/script.js
+++ NavBar/script.js
@@ -1,20 +1,32 @@
+let menu = document.querySelector("#nav i");
+let cross = document.querySelector("#full i");
+
 let tl = gsap.timeline();
 
-tl.to("#full",{
-    right: 0,
-    duration: 0.5
-})
+tl.to("#full", {
+  right: 0,
+  duration: 0.5,
+});
 
-tl.from("#full h4",{
-    x:600,
-    opacity: 0,
-    duration: 0.7,
-    stagger:0.1
-})
+tl.from("#full h4", {
+  x: 600,
+  opacity: 0,
+  duration: 0.7,
+  stagger: 0.1,
+});
 
-tl.from("#full i",{
-    x:100,
-    duration:0.2,
-    opacity: 0
-})
+tl.from("#full i", {
+  x: 100,
+  duration: 0.2,
+  opacity: 0,
+});
 
+tl.pause();
+
+menu.addEventListener("click", function () {
+  tl.play();
+});
+
+cross.addEventListener("click", function () {
+  tl.reverse();
+});
