55ee4bd2f87937c4e606df72529ac59953bb9f23
vidhyasagar <vs1522250@gmail.com>
2026-04-29 19:20:04 +0530

    custom cursor

--- custome-cursor/script.js
+++ custome-cursor/script.js
@@ -1,11 +1,28 @@
-let main = document.querySelector("#main")
-let cursor = document.querySelector("#cursor")
+let main = document.querySelector("#main");
+let cursor = document.querySelector("#cursor");
+let imageDiv = document.querySelector("#image");
 
-main.addEventListener("mousemove",function(dets){
-    gsap.to(cursor,{
-        x:dets.x,
-        y:dets.y,
-        duration:0.5,
-        ease:"back.out"
-    })
-})
+main.addEventListener("mousemove", function (dets) {
+  gsap.to(cursor, {
+    x: dets.x,
+    y: dets.y,
+    duration: 0.5,
+    ease: "back.out",
+  });
+});
+
+imageDiv.addEventListener("mouseenter", function () {
+  cursor.innerHTML = "Viwe More";
+  gsap.to(cursor, {
+    scale: 4,
+    backgroundColor: "rgba(255, 255, 255, 0.41)"
+  });
+});
+
+imageDiv.addEventListener("mouseleave", function () {
+  cursor.innerHTML = "";
+  gsap.to(cursor, {
+    scale: 1,
+    backgroundColor: ""
+  });
+});
