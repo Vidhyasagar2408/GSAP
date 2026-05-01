b70f1322520a2e4ace09feecd995461978311776
vidhyasagar <vs1522250@gmail.com>
2026-05-01 17:09:07 +0530

    Animated react

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,35 +2,35 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import React, { useRef, useState } from "react";
 const App = () => {
-  const boxRef = useRef();
+  const imageRef = useRef();
 
   const [xValue, setXValue] = useState(0);
+  const [yValue, setYValue] = useState(0);
   const [rotate, setRotate] = useState(0);
 
   const randomX = gsap.utils.random(-500, 500, 100);
+  const randomY = gsap.utils.random(-400, 400, 100);
   const randomR = gsap.utils.random(-720, 7200, 30);
 
   useGSAP(() => {
-    gsap.to(boxRef.current, {
+    gsap.to(imageRef.current, {
       x: xValue,
+      y: yValue,
       rotate: rotate,
       duration: 1,
     });
-  }, [xValue, rotate]);
+  }, [xValue, yValue, rotate]);
 
   return (
     <div>
       <main>
-        <button
+        <img
           onClick={() => {
             setXValue(randomX);
+            setYValue(randomY);
             setRotate(randomR);
           }}
-        >
-          Animate
-        </button>
-        <img
-          ref={boxRef}
+          ref={imageRef}
           src="https://pngimg.com/uploads/fly/fly_PNG3946.png"
           alt=""
         />
